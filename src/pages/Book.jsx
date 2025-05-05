c8332ee68142454486ef8a863f1c5a28dd728fe7
1Dire <d01038145300@nate.com>
2025-05-06 01:53:25 +0900

    상세배경이미지 추가

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -5,7 +5,22 @@ import RoomNotice from "@/components/layout/room/RoomNotice";
 
 const Book = () => {
   return (
-    <section id="book" className="h-screen pt-20">
+    <section id="book" className="h-screen pt-[72px]">
+      {/* Hero Section (상단 배경 이미지) */}
+      <div
+        className="relative h-96 bg-cover bg-center"
+        style={{
+          backgroundImage: "url('https://picsum.photos/1200/600')", // 랜덤 배경 이미지 URL
+        }}
+      >
+        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
+        <div className="relative text-center text-white py-24">
+          <h1 className="text-4xl font-extrabold">예약 안내</h1>
+          <p className="mt-4 text-xl">편안한 휴식, 지금 예약하세요.</p>
+        </div>
+      </div>
+
+      {/* 방 예약 섹션 */}
       <div className="py-6 px-4 sm:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto">
           <RoomNotice />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";  // Link 임포트
+
 const Home = () => {
   return (
     <>
       <section id="home" className="h-screen bg-cover bg-center pt-18">
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-55"></div>
+        <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-65"></div>
         <div className="relative px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56 z-10">
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
             고요한 자연 속에서의 완벽한 쉼터 <br />
@@ -15,13 +17,13 @@ const Home = () => {
             환영합니다.
           </p>
           <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-            <a
-              href="#"
+            <Link
+              to="/book"  
               className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-yellow-800 hover:bg-yellow-700 focus:ring-4 focus:ring-yellow-800"
             >
               예약안내
               <FaArrowRight className="w-3.5 h-3.5 ms-2 rtl:rotate-180" />
-            </a>
+            </Link>
             <a
               href="#"
               className="inline-flex justify-center hover:text-yellow-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
