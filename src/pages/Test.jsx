870d079da34526aea424e4d5207d021a92f67818
1Dire <d01038145300@nate.com>
2025-05-14 15:37:18 +0900

    테스트 수정

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect } from 'react';
-import { useLoading } from "@/context/LoadingContext";  // 로딩 상태 가져오기
-import { Loder } from "@/components/util/Loder";  // 로딩 컴포넌트
-import Grid from '../components/util/grid/grid';
-const Test = () => {
+import React, { useEffect } from "react";
+import { useLoading } from "@/context/LoadingContext"; // 로딩 상태 가져오기
+import { Loder } from "@/components/util/Loder"; // 로딩 컴포넌트
 
+const Test = () => {
   return (
     <section id="test" className="pt-[72px] bg-gray-50">
-      <Grid/>
+      테스트
     </section>
   );
 };
