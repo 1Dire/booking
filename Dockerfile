b2b23cd18fdf3e020c5d58e92ca6d9414ac7b27f
1Dire <d01038145300@nate.com>
2025-05-14 19:37:09 +0900

    업데이트

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-# 1. Node.js 이미지 사용
-FROM node:16 AS build
+# 1. Node.js v23.11.0 이미지 사용
+FROM node:23.11.0 AS build
 
 # 2. 작업 디렉토리 설정
 WORKDIR /app
@@ -12,14 +12,6 @@ RUN npm install
 COPY . ./
 RUN npm run build
 
-# 5. Nginx를 위한 설정
-FROM nginx:alpine
-
-# 6. 빌드된 파일을 Nginx 서버로 복사
-COPY --from=build /app/dist /usr/share/nginx/html
-
-# 7. Nginx 설정 파일을 덮어쓰기
-COPY nginx.conf /etc/nginx/nginx.conf
 
 # 8. Nginx 포트 노출
 EXPOSE 80
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import flowbiteReact from "flowbite-react/plugin/vite"
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import flowbiteReact from "flowbite-react/plugin/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),  // '@'를 src 폴더로 설정
-      crypto: 'crypto-browserify', // crypto 모듈을 브라우저 폴리필로 설정
-    }
+      "@": path.resolve(__dirname, "src"), // '@'를 src 폴더로 설정
+    },
   },
   server: {
-    host: '0.0.0.0', // 모든 네트워크 인터페이스에 바인딩
-    port: 5173, // 포트는 기본값으로 두었지만, 원하는 포트로 변경 가능
-    open: true, // 서버 시작 시 자동으로 브라우저 열기 (선택사항)
-  }
-})
+    host: true, // 모든 네트워크 인터페이스에 바인딩
+    port: 5173,
+    strictPort: true,
+    open: true,
+  },
+});
